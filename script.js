add14dab121816af8ac4dcc9473e59f9465d7b7f
yugmani <yugmani@yahoo.com>
2021-06-09 13:45:33 -0700

    RegExp.prototype.test() and RegExp.prototype.exec();

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// ******************************************
 // 1. Working with regular expressions
+// ******************************************
 
 // In JavaScript there are 2 ways to going about this:
 
 // A. Constructor
+// ******************************************
+
 const regex = new RegExp('Hello');
 const result = regex.test('Hello World!');
 
@@ -10,28 +14,54 @@ const result = regex.test('Hello World!');
 // Expected: true
 
 // B. Literal
+// ******************************************
+
 const match = /Hello/;
 const output = match.test('Hello World!');
 
 // console.log(output);
 // Expected: true
 
-// In both scenarios, the variables 'regex' and 'match' are objects. 
+// In both scenarios, the variables 'regex' and 'match' are objects.
 // However, the first example has a more familiar look, instancing an object with a `string` as a parameter.
 // In the second scenario things look a bit weird, there is something that resembles a `string` but instead of quotes is wrapped in `/`.
-//  As it turns out both ways represent the same. 
+//  As it turns out both ways represent the same.
 
-// 2.
-// RegExp.prototype.test()
+// ******************************************
+// 2. RegExp.prototype.test()
+// ******************************************
+
 // The test() method executes a search for a match between a regular expression and a specified string. Returns `true` or `false`.
 
-const regexExpression = (str)=>{
+const regexExpression = str => {
+  const regex = RegExp('foo');
+  return regex.test(str);
+};
 
-const regex = RegExp('foo')
-console.log(regex.test(str));
-}
+// console.log(regexExpression('table football'));     //true
 
-console.log(regexExpression('table football'));
+// ******************************************
+// 3. RegExp.prototype.exec()
+// ******************************************
 
+// The `exec(`) method executes a search for a match in a specified string. Returns a result array, or null.
 
+const regexExec = str => {
+  const regex = /foo/g;
+  let output = '';
+  let result;
+  while ((result = regex.exec(str)) !== null) {
+    output += `Found ${result[0]} at ${result.index}.\n`;
+  }
+  return output;
+};
 
+// console.log(regexExec('table football, foosball'));
+// Found foo at 6.
+// Found foo at 16.
+
+// ******************************************
+
+// ******************************************
+
+// ******************************************
