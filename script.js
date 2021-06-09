8a8546adbc95ec8650c2aae48f7eafe50b59bd05
yugmani <yugmani@yahoo.com>
2021-06-09 14:03:19 -0700

    match and matchAll regex functions

--- script.js
+++ script.js
@@ -61,7 +61,36 @@ const regexExec = str => {
 // Found foo at 16.
 
 // ******************************************
-
+// 4. String.prototype.match()
 // ******************************************
 
+// The `match()` method retrieves the result of matching a string against a regular expression.
+const matchString = str => {
+  const regex = /[A-Z]/g;
+  return (found = str.match(regex));
+};
+
+const paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
+// console.log(matchString(paragraph));  // ["T", "I"]
+
 // ******************************************
+// 5. String.prototype.matchAll()
+// ******************************************
+
+// The matchAll() method returns an iterator of all results matching a string against a regular expression, including capturing groups.
+
+const regexMatchAll = (str)=>{
+  const regex = /t(e)(st(\d?))/g;
+  return arr = [...str.matchAll(regex)]
+}
+
+// console.log(regexMatchAll('test1')); 
+// ["test1", "e", "st1", "1", index: 0, input: "test1", groups: undefined]
+
+// console.log(regexMatchAll('test1test2')); 
+// (2) [Array(4), Array(4)]
+// 0: (4) ["test1", "e", "st1", "1", index: 0, input: "test1test2", groups: undefined]
+// 1: (4) ["test2", "e", "st2", "2", index: 5, input: "test1test2", groups: undefined]
+// length: 2
+// __proto__: Array(0)
+
