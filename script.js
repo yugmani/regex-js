fff7ee74469fc4343c672c20d5f1ce0f2be2e278
yugmani <yugmani@yahoo.com>
2021-06-09 14:15:25 -0700

    string.prototype.search()

--- script.js
+++ script.js
@@ -77,20 +77,37 @@ const paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
 // 5. String.prototype.matchAll()
 // ******************************************
 
-// The matchAll() method returns an iterator of all results matching a string against a regular expression, including capturing groups.
+// The `matchAll()` method returns an iterator of all results matching a string against a regular expression, including capturing groups.
 
-const regexMatchAll = (str)=>{
+const regexMatchAll = str => {
   const regex = /t(e)(st(\d?))/g;
-  return arr = [...str.matchAll(regex)]
-}
+  return (arr = [...str.matchAll(regex)]);
+};
 
-// console.log(regexMatchAll('test1')); 
+// console.log(regexMatchAll('test1'));
 // ["test1", "e", "st1", "1", index: 0, input: "test1", groups: undefined]
 
-// console.log(regexMatchAll('test1test2')); 
+// console.log(regexMatchAll('test1test2'));
 // (2) [Array(4), Array(4)]
 // 0: (4) ["test1", "e", "st1", "1", index: 0, input: "test1test2", groups: undefined]
 // 1: (4) ["test2", "e", "st2", "2", index: 5, input: "test1test2", groups: undefined]
 // length: 2
 // __proto__: Array(0)
 
+// ******************************************
+// 6. String.prototype.search()
+// ******************************************
+
+// The `search()` method executes a search for a match between a regular expression and this string object. It returns the index at which the matched happened, or -1 if there is no match.
+
+// Find the position of any character that is not a word character or white space
+const regexSearch = str => {
+  const regex = /[^\w\s]/g;
+  return str.search(regex);
+};
+
+const text =
+  'The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?';
+
+// console.log(regexSearch(text)); // 43; position of dot (.);
+// 61 position of (?) after omitting dot(.);
